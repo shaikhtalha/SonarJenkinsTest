a0166a6b34b1a5252505816c9a515f5caa1d8330
talhas <talha.s@arnekt.com>
2019-11-22 16:50:38 +0530

    Project

---
+++ src/main/java/com/codeusingjava/App.java
@@ -0,0 +1,13 @@
+package com.codeusingjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
